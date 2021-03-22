0b91eb1595a56669b5f15dd5c33987ecf9a6045f
Alex Mourtos <amourtos@gmail.com>
2021-03-22 14:22:44 -0400

    added bootstrap to message list

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 import { useStore, LIKES } from "../store/store";
 import { likeRequest } from "../fetchRequests";
+import { Card, Button } from "react-bootstrap";
+import Moment from "moment";
+import User from "../components/UserCard/User.js";
 
 function MessageItem(props) {
   const dispatch = useStore((state) => state.dispatch);
@@ -15,12 +19,31 @@ function MessageItem(props) {
   };
   return (
     <div>
-      {props.username},{props.text},{props.id},{props.createdAt} 
-      <button className="Likes" onClick={handleLikes}>
-          like
-        </button>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>{props.username}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{Moment(props.createdAt).format("MM-DD-YYYY")}</Card.Subtitle>
+          <Card.Text>{props.text}</Card.Text>
+          <Card.Link to="#">
+            <Link to={`users/${props.username}`} render={(props) => <User {...props} />}>
+              Go to profile
+            </Link>
+          </Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+          <Button className="Likes" onClick={handleLikes}>
+            like
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
 
 export default MessageItem;
+
+{
+  /* {props.username},{props.text},{props.id},{props.createdAt} 
+      <button className="Likes" onClick={handleLikes}>
+          like
+</button>*/
+}
