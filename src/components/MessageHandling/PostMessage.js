93f8f1b6f047f2984709a1cc589a588da1cfb98d
lindsey-fisher <ellefish30@gmail.com>
2021-03-24 10:21:08 -0400

    PostMessage update

--- src/components/MessageHandling/PostMessage.js
+++ src/components/MessageHandling/PostMessage.js
@@ -4,10 +4,17 @@ import {POST_MESSAGE, useStore} from '../../store/store.js';
 
 
 const PostMessage = (props) => {
-   const user = useStore((state) => state.user)
+   let user = useStore((state) => state.user)
    const [formData, setFormData] = useState({
     text: ""
   });
+  
+  const handleChange = (e) => {
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+    setFormData((state) => ({ ...state, [inputName]: inputValue }));
+  };
+  
   
   const handlePostMessage = (e) => {
      e.preventDefault();
@@ -19,29 +26,23 @@ const PostMessage = (props) => {
         <PopUp>
           <h3>Message Posted</h3>
         </PopUp>;
-     
          });
-       },
+        })} 
+     
+      
+      return (
+        <div>
+            <form id="PostMessage" onSubmit={handleSubmit}></form>
+            <label>Post Message</label>
+        <input type="text" name="username" autoFocus value={postMessage.username} required onChange={handleChange}></input>
+        
+        </div>
+    )
+}
+     
+   // use Aux tag or react fragment instead of div tags ^^^^
+ 
+ export default PostMessage;
        
-       const handleChange = (e) => {
-         const inputName = e.target.name;
-         const inputValue = e.target.value;
-         setFormData((state) => ({ ...state, [inputName]: inputValue }));
-       };
        
-       return (
-           <div>
-               <form id="PostMessage" onSubmit={handleSubmit}></form>
-               <label>Post Message</label>
-           <input type="text" name="username" autoFocus value={postMessage.username} required onChange={handleChange}></input>
-           
-           </div>
-       )
-   }
-
-  // use Aux tag or react fragment instead of div tags ^^^^
-
-export default PostMessage
-
-
 
