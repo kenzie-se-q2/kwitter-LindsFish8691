4f9c6ac8ce41ce48774b5a3d19d505c0693cfa61
amourtos <amourtos@gmail.com>
2021-03-28 16:05:39 -0400

    added feature for pictures from other members information

--- src/components/UserCard/User.js
+++ src/components/UserCard/User.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { getUser } from "../../fetchRequests.js";
-import { Card, ListGroupItem, ListGroup, Button } from "react-bootstrap";
+import { baseURL, getUser } from "../../fetchRequests.js";
+import { Card, ListGroupItem, ListGroup, Button, Image } from "react-bootstrap";
 import Moment from "moment";
 import Menu from "../Menu.js";
 
@@ -16,13 +16,18 @@ const User = ({ match }) => {
       <Menu />
       <Card style={{ width: "18rem" }}>
         {/* <Card.Img variant="top" src={user.pictureLocation} /> */}
+        <Image src={`${baseURL}users/${user.username}/picture`} rounded />
         <Card.Body>
           <Card.Title>Username: {user.username}</Card.Title>
           <Card.Title>display Name: {user.displayName}</Card.Title>
         </Card.Body>
         <ListGroup className="list-group-flush">
-          <ListGroupItem>Created On:{Moment(user.createdAt).format("MM-DD-YYYY")}</ListGroupItem>
-          <ListGroupItem>Updated on:{Moment(user.updatedAt).format("MM-DD-YYYY")}</ListGroupItem>
+          <ListGroupItem>
+            Created On:{Moment(user.createdAt).format("MM-DD-YYYY")}
+          </ListGroupItem>
+          <ListGroupItem>
+            Updated on:{Moment(user.updatedAt).format("MM-DD-YYYY")}
+          </ListGroupItem>
           <ListGroupItem>Google ID: {user.googleId}</ListGroupItem>
         </ListGroup>
         <Card.Body>
